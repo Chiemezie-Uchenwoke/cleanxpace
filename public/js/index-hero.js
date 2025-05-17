8cb07c2292455a8febd1ec2794f1328cdeed5f24
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-05-17 15:25:44 +0100

    complete hero and add services section

---
+++ public/js/index-hero.js
@@ -0,0 +1,17 @@
+const images = ["images/home_1.jpg", "images/man_cleaning.jpg", "images/clean_1.jpg", "images/sofa_2.jpg"];
+const heroContainer = document.getElementById("hero");
+let index = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    let imageUrl = images[index];
+    heroContainer.style.backgroundImage = `url(${imageUrl})`;
+
+    const handleImage = () => {
+        index = (index + 1) % images.length;
+        imageUrl = images[index];
+        heroContainer.style.backgroundImage = `url(${imageUrl})`;
+        heroContainer.style.transition = `background-image 0.3s ease-in-out`;
+    }
+
+    setInterval(handleImage, 5000);
+});
