669e8d37579dd6702b3ec267c05061f7035b4b21
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-01-26 09:00:54 +0100

    Fix: Remove headers section in API request

--- public/js/contact.js
+++ public/js/contact.js
@@ -13,10 +13,6 @@ contactForm.addEventListener("submit", async (e) => {
     try {
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
             body: formData
         });
 
